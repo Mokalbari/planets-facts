f31045f89d1c2ab7be4d0043c92b8d555640987d
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-06-18 22:55:01 +0200

    add useState to manage planets + setPlanet in navBar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,37 +1,39 @@
 import '../styles/Navbar.modules.css'
+import data from '../data/data.json'
 
-const Navbar: React.FC = () => (
-  <header className="header flex-row">
-    <h2>The planets</h2>
-    <nav>
-      <menu className="header__menu flex-row">
-        <li>
-          <h4>Mercury</h4>
-        </li>
-        <li>
-          <h4>Venus</h4>
-        </li>
-        <li>
-          <h4>Earth</h4>
-        </li>
-        <li>
-          <h4>Mars</h4>
-        </li>
-        <li>
-          <h4>Jupiter</h4>
-        </li>
-        <li>
-          <h4>Saturn</h4>
-        </li>
-        <li>
-          <h4>Uranus</h4>
-        </li>
-        <li>
-          <h4>Neptune</h4>
-        </li>
-      </menu>
-    </nav>
-  </header>
-)
+interface Navbar {
+  setPlanet: (string: string) => void
+}
+
+const Navbar: React.FC<Navbar> = ({ setPlanet }) => {
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    const value: string = (event.target as HTMLElement).innerText
+    const formatedValue: string = value.charAt(0) + value.slice(1).toLowerCase()
+    setPlanet(formatedValue)
+  }
+
+  const handleKey = () => console.log('Key not yet supported')
+
+  return (
+    <header className="header flex-row">
+      <h2>The planets</h2>
+      <nav>
+        <menu
+          className="header__menu flex-row"
+          onClick={handleClick}
+          onKeyUp={handleKey}
+        >
+          {data.map(planet => {
+            return (
+              <li key={planet.name}>
+                <h4>{planet.name}</h4>
+              </li>
+            )
+          })}
+        </menu>
+      </nav>
+    </header>
+  )
+}
 
 export default Navbar
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,10 +2,14 @@ import Navbar from '../components/Navbar'
 import MainHeroContainer from '../components/MainHeroContainer'
 import Information from '../components/Information'
 
+import { useState } from 'react'
+
 const App: React.FC = () => {
+  const [planet, setPlanet] = useState('Earth')
+
   return (
     <>
-      <Navbar />
+      <Navbar setPlanet={setPlanet} />
       <MainHeroContainer />
       <Information />
     </>
