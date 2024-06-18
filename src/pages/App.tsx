912453dbe027343b67a1075050ef5d3abefbb4ba
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-06-18 23:57:46 +0200

    add types and state to app

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,17 +1,27 @@
 import Navbar from '../components/Navbar'
 import MainHeroContainer from '../components/MainHeroContainer'
 import Information from '../components/Information'
-
 import { useState } from 'react'
+import data from '../data/data.json'
+import type { Planet } from '../types/Planet'
 
 const App: React.FC = () => {
-  const [planet, setPlanet] = useState('Earth')
+  const [planet, setPlanet] = useState<string>('Earth')
+  const [information, setInformation] = useState<string>('overview')
+  const currentPlanet: Planet | undefined = data.find(
+    (element: Planet) => element.name === planet,
+  )
 
   return (
     <>
       <Navbar setPlanet={setPlanet} />
-      <MainHeroContainer />
-      <Information />
+      <MainHeroContainer
+        planet={planet}
+        information={information}
+        setInformation={setInformation}
+        currentPlanet={currentPlanet}
+      />
+      <Information information={information} />
     </>
   )
 }
